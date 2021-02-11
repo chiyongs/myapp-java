da4e07cd8086d6130e9e5dc5c988d619535a5fd1
chiyongs <tlscldyd@gmail.com>
2021-02-11 23:55:07 +0900

    Create AccountingApp

---
+++ src/AccountingApp.java
@@ -0,0 +1,23 @@
+public class AccountingApp {
+    public static void main(String[] args){
+
+        double valueOfSupply = Double.parseDouble(args[0]);
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double VAT = valueOfSupply * vatRate;
+        double total = valueOfSupply + valueOfSupply * vatRate;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+        double dividend1 = income * 0.5;
+        double dividend2 = income * 0.3;
+        double dividend3 = income * 0.2;
+        System.out.println("Value of Supply : "+valueOfSupply);
+        System.out.println("VAT : "+ VAT);
+        System.out.println("Total : "+ total);
+        System.out.println("Expense : "+ expense);
+        System.out.println("Income : "+ income);
+        System.out.println("Dividend 1: "+dividend1);
+        System.out.println("Dividend 1: "+dividend2);
+        System.out.println("Dividend 1: "+dividend3);
+    }
+}
